6c2d01d025e67957698a78eafa36dc2b29b4e6ed
Bence Papp <bencepapp1101@gmail.com>
2022-12-14 18:27:33 +0100

    🚀 Profile view

---
+++ TwitterClone/Core/Profile/ViewModels/TweetFilterViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  TweetFilterViewModel.swift
+//  TwitterClone
+//
+//  Created by Bence Papp on 2022. 12. 14..
+//
+
+import Foundation
+
+enum TweetFilterViewModel: Int, CaseIterable {
+    case tweets
+    case replies
+    case likes
+
+    var title: String {
+        switch self {
+        case .tweets: return "Tweets"
+        case .replies: return "Replies"
+        case .likes: return "Likes"
+        }
+    }
+}
