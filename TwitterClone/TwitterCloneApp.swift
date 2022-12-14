af5e500a25727570f4bdc7e15acf5b353b010cce
Bence Papp <bencepapp1101@gmail.com>
2022-12-14 09:15:48 +0100

    🚀 init

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by Bence Papp on 2022. 12. 14..
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
