29e9f762e456da7cb00360a354bcb0711f7732ae
Bence Papp <bencepapp1101@gmail.com>
2022-12-15 14:05:27 +0100

    🚀 TextArea created

--- TwitterClone/Core/Feed/Views/FeedView.swift
+++ TwitterClone/Core/Feed/Views/FeedView.swift
@@ -8,12 +8,33 @@
 import SwiftUI
 
 struct FeedView: View {
+    @State private var showNewTweetView = false
+
     var body: some View {
-        ScrollView {
-            LazyVStack {
-                ForEach(0 ... 20, id: \.self) { _ in
-                    TweetsRowView()
+        ZStack(alignment: .bottomTrailing) {
+            ScrollView {
+                LazyVStack {
+                    ForEach(0 ... 20, id: \.self) { _ in
+                        TweetsRowView()
+                    }
                 }
+            }
+
+            Button {
+                showNewTweetView.toggle()
+            } label: {
+                Image(systemName: "square.and.pencil")
+                    .resizable()
+                    .renderingMode(.template)
+                    .frame(width: 28, height: 28)
+                    .padding()
+            }
+            .background(Color(.systemBlue))
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .padding()
+            .fullScreenCover(isPresented: $showNewTweetView) {
+                NewTweetView()
             }
         }
     }
